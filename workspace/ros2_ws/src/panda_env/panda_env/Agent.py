43315d5661463c1f24c694458cbfd0cce6c26a44
Nicola-Taddei <nicola.taddei01@gmail.com>
2024-07-29 02:36:31 +0200

    Merged code

--- workspace/ros2_ws/src/panda_env/panda_env/Agent.py
+++ workspace/ros2_ws/src/panda_env/panda_env/Agent.py
@@ -6,11 +6,16 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from torchvision import transforms
 import os
+import torch.nn.functional as F
 
 
 class Params:
     patch_size = 24
     theta_resolution = 180
+    l = 1
+    margin = 0
+    gamma = 0.99
+    w = 0.5
 
 class Agent:
     def __init__(self):
@@ -56,20 +61,105 @@ class Agent:
         j = torch.argmax(q2_map).item()
         return u,v,j
 
-    def l(a,a_e):
-        pass
+    def l(self, a: torch.Tensor, a_e: torch.Tensor) -> torch.Tensor:
+        '''the non expert action penalty
+        a: tensor with actions of the agent
+        a_e: tensor with actions of the expert
+        
+        return l: the loss of the non expert
+        '''
+        # large margin loss penalty for non-expert actions
+        # https://arxiv.org/pdf/1704.03732
+        l = Params.l * F.relu(a - a_e + Params.margin)
+        return l
 
-    def lossSLM(s,a_e):
-        pass
+    def lossTD1(self, s: tuple, a: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        '''calculate the first TD loss for Q1
+        s: it's a tuple of 3 dimension containing 3 tensor, one for the 
+            height_images, one for the in hand images and one for the
+            boolean related to the gripper status
+            
+        a: tensor of actions
+        y: TD target
+        
+        return the TD loss for Q1
+        '''
+        Q1_pred = self.Q1(s[0], s[1], s[2])[:, a]
+        loss = F.huber_loss(Q1_pred, y)
+        return loss
 
-    def lossTD1(s1,a1):
-        pass
+    def lossTD2(self, s: tuple, a: torch.Tensor, e: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        '''calculate the first TD loss for Q2
+        s: it's a tuple of 3 dimension containing 3 tensor, one for the 
+            height_images, one for the in hand images and one for the
+            boolean related to the gripper status
+        
+        e: tensor for encoding
+            
+        a: tensor of actions
+        y: TD target
+        
+        return the TD loss for Q2
+        '''
 
-    def lossTD2(s2,a2):
-        pass
+        Q2_pred = self.Q2(s[0], s[1], s[2], e)[:, a]
+        loss = F.huber_loss(Q2_pred, y)
+        return loss
 
-    def train(self, dataset):
-        pass
+    def lossSLM(self, s: tuple, a_e: torch.Tensor) -> torch.Tensor:
+        '''calculate the SLM loss
+        s: it's a tuple of 3 dimension containing 3 tensor, one for the 
+            height_images, one for the in hand images and one for the
+            boolean related to the gripper status
+            
+        a: tensor of actions
+        a_e: tensor of expert actions
+        y: TD target
+        
+        return the TD loss for Q2
+        '''
+
+        # TODO: recheck tomorrow, we don't need the action a
+
+        # Calculating the strict large margin loss (SLM)
+        margin_loss = self.l(a, a_e)
+        e = self.Q1(s[0], s[1], s[2])[:, 0]
+        Q_s_a1 = self.Q1(s[0], s[1], s[2])[:, 1] 
+        #I take the Q values at the expert position
+        Q_s_a_e1 = self.Q1(s[0], s[1], s[2])[a_e[:,0:2]][:, 1]
+        a1 = torch.argmax(Q_s_a1)
+        Q_s_a = self.Q2(s[0], s[1], s[2], e, a1)  # Assuming Q is a network attribute
+        Q_s_a_e = self.Q2(s[0], s[1], s[2], e, a_e[:,0:2])
+        index = Q_s_a[Q_s_a > Q_s_a_e - margin_loss]
+        LSLM = torch.mean(Q_s_a[index] + self.l(a, a_e)[index] - Q_s_a_e[index])
+        return LSLM
+
+    def train(self, De: torch.Torch):
+        #Pre_training for M
+        s = De[0]
+        sn = De[2]
+        a_e = De[1]
+        r = De[4]
+        #a1 are the x,y coordinates
+        a1_n = torch.argmax(self.Q1_target(sn[0], sn[1], sn[2])[:, 1])
+        #a2 is the rotation angle
+        e = self.Q1_target(sn[0], sn[1], sn[2])[:, 0]
+        a2_n = torch.argmax(self.Q2_target(sn[0], sn[1], sn[2], e, a1_n))
+        y = r + Params.gamma*torch.max(a2)
+
+        a1 = torch.argmax(self.Q1(s[0], s[1], s[2])[:, 1])
+        e = self.Q1(s[0], s[1], s[2])[:, 0]
+        a2 = torch.argmax(self.Q2(sn[0], s[1], s[2], e, a2))
+
+        ltd_loss = self.lossTD1(s, a1, y) + self.lossTD2(s, torch.concatenate(a1, a2), e, y)
+        lslm = self.LSLM(s,torch.concatenate(a1, a2), a_e)
+        # w = something
+        L = ltd_loss + Params.w* lslm
+        # gradient descent
+
+        # Training for N:
+        # Copy some part before and self play
+        #  better understand how to talk with the env
 
 
 def read_csv(path):
